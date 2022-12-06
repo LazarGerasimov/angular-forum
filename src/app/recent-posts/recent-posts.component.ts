e9888ee66617f2fc81bdf4e88cfa34395d8ead18
Laz <lazgerasimov@gmail.com>
2022-12-06 09:52:12 +0000

    .

--- src/app/recent-posts/recent-posts.component.ts
+++ src/app/recent-posts/recent-posts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { IPost } from '../interfaces/post';
 
 @Component({
   selector: 'app-recent-posts',
@@ -8,12 +9,14 @@ import { ApiService } from '../api.service';
 })
 export class RecentPostsComponent implements OnInit {
 
+  posts: IPost[] | null = null;
+
   constructor (private apiService: ApiService) {}
 
   ngOnInit(): void {
     this.apiService.loadPosts(5).subscribe({
       next: (value) => {
-        console.log(value)
+        this.posts = value;
       },
       error: (err) => {
         console.error(err)
