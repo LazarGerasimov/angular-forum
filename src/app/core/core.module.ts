73ad4b98cb2e0bbbc6702487708ba6b8e3de1427
Laz <lazgerasimov@gmail.com>
2022-12-07 12:02:18 +0000

    add home comp

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,6 +5,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
 
 
@@ -13,6 +14,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     HeaderComponent,
     FooterComponent,
     PageNotFoundComponent,
+    HomeComponent,
   ],
   imports: [
     CommonModule,
