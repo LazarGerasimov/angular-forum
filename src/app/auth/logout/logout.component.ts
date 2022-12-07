1736b1b72a59d3a7d596ff24f8901ff04b9b0621
Laz <lazgerasimov@gmail.com>
2022-12-07 09:34:20 +0000

    add logout

--- src/app/auth/logout/logout.component.ts
+++ src/app/auth/logout/logout.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-logout',
@@ -7,4 +10,8 @@ import { Component } from '@angular/core';
 })
 export class LogoutComponent {
 
+  constructor(private router: Router, private authService: AuthService) {
+    this.authService.user = null;
+    this.router.navigate(['/']);
+  }
 }
