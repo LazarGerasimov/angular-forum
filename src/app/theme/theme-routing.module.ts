7b82e8d661794e2d79deb232764c2663a9bbd2f7
Laz <lazgerasimov@gmail.com>
2022-12-07 11:21:20 +0000

    add htmls

--- src/app/theme/theme-routing.module.ts
+++ src/app/theme/theme-routing.module.ts
@@ -1,6 +1,6 @@
 import { RouterModule, Routes } from "@angular/router";
 import { NewThemeComponent } from "./new-theme/new-theme.component";
-import { ThemeListComponent } from "./theme-list/theme-list.component";
+//import { ThemeListComponent } from "./theme-list/theme-list.component";
 
 
 const routes: Routes = [
