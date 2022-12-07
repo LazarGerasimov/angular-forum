9c8306dbbde47f22766f75e16e80b9f4ba8b6b07
Laz <lazgerasimov@gmail.com>
2022-12-07 09:10:17 +0000

    authService

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { LogoutComponent } from './logout/logout.component';
 
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    LogoutComponent
   ],
   imports: [
     CommonModule,
