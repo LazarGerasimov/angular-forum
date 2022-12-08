89e560360e00daf14d21cbe59f88c2b7f1e831d5
Laz <lazgerasimov@gmail.com>
2022-12-08 10:43:59 +0000

    .

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -9,12 +9,15 @@ import { AuthService } from '../auth.service';
 })
 export class LoginComponent {
 
-  constructor(private router: Router, private authService: AuthService) {
+  constructor(private activatedRoute: ActivatedRoute , private router: Router, private authService: AuthService) {}
+
+  loginHandler(): void {
     this.authService.user = {
       username: 'John'
     } as any;
-    this.router.navigate(['/']);
   }
 
+   
 
+   
 }
--- src/app/shared/guards/auth.activate.ts
+++ src/app/shared/guards/auth.activate.ts
@@ -3,20 +3,18 @@ import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTr
 import { Observable } from "rxjs";
 import { AuthService } from "src/app/auth/auth.service";
 
-
-
 @Injectable({
     providedIn: 'root'
 })
 
 export class AuthActivate implements CanActivate {
 
-    constructor(private authService: AuthService, private router: Router) {}
+    constructor(private authService: AuthService, private router: Router) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
         const loginRequired = route.data['loginRequired'];
-        if (loginRequired === undefined || this.authService.isLoggedIn === loginRequired) {return true;};
-        return this.router.createUrlTree([`/auth/login?returnUrl=${route.url}`]);
+        if (loginRequired === undefined || this.authService.isLoggedIn === loginRequired) { return true; };
+        const returnUrl = route.url.map(u => u.path).join('/');
+        return this.router.createUrlTree(['/auth/login'], { queryParams: { returnUrl } });
     }
-
 }
