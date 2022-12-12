0f478cbc9617d5736e782a230764e933eb683526
Laz <lazgerasimov@gmail.com>
2022-12-12 09:04:17 +0000

    .

--- src/app/auth/profile/profile.component.ts
+++ src/app/auth/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { appEmailDomains } from 'src/app/shared/constants';
+import { appEmailValidator } from 'src/app/shared/validators';
 
 @Component({
   selector: 'app-profile',
@@ -9,7 +11,10 @@ import { FormBuilder } from '@angular/forms';
 export class ProfileComponent {
 
   form = this.fb.group({
-    
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, appEmailValidator(appEmailDomains)]],
+    ext: [''],
+    tel: [''],
   })
 
   constructor (private fb: FormBuilder) {}
