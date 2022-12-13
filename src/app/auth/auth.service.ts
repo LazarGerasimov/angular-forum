3b64b23071e4301f65872431b43990d53507e8cf
Laz <lazgerasimov@gmail.com>
2022-12-13 12:24:17 +0000

    .

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,11 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, OnDestroy } from '@angular/core';
-import { BehaviorSubject, filter, Subscription, tap } from 'rxjs';
+import { Injectable } from '@angular/core';
 import { IUser } from '../shared/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService implements OnDestroy{
-
-  private user$$ = new BehaviorSubject<undefined | null | IUser>(undefined);
-  user$ = this.user$$.asObservable();
+export class AuthService {
 
   user: IUser | null = null;
 
@@ -17,37 +13,21 @@ export class AuthService implements OnDestroy{
     return this.user !== null;
   }
 
-  subscription: Subscription;
-
-  constructor(private http: HttpClient) { 
-    this.subscription = this.user$.pipe(
-      filter((val): val is IUser | null => val != undefined))
-      .subscribe(user => {
-      this.user = user;
-    })
-  }
+  constructor(private http: HttpClient) { }
 
   register(username: string, email: string, password: string, rePassword: string, tel?: string) {
-    return this.http.post<IUser>('/api/register', { username, email, password, rePassword, tel })
-    .pipe(tap(user => this.user$$.next(user)));
+    return this.http.post<IUser>('/api/register', { username, email, password, rePassword, tel });
   }
 
   login(email: string, password: string) {
-    return this.http.post<any>('/api/login', { email, password })
-    .pipe(tap(user => this.user$$.next(user)));;
+    return this.http.post<any>('/api/login', { email, password });
   }
 
   logout() {
-    return this.http.post<void>('/api/logout', {})
-    .pipe(tap(() => this.user$$.next(null)));;
+    return this.http.post<void>('/api/logout', {});
   }
 
   getProfile() {
-    return this.http.get<IUser>('/api/users/profile')
-    .pipe(tap(user => this.user$$.next(user)));;
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    return this.http.get<IUser>('/api/users/profile');
   }
 }
