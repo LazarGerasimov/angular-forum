81e39c2ad254242f4e4e17a0a74dcd41e61ee1cc
Laz <lazgerasimov@gmail.com>
2022-12-13 08:48:28 +0000

    .

--- src/app/app.interceptor.ts
+++ src/app/app.interceptor.ts
@@ -1,6 +1,9 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Injectable, Provider } from "@angular/core";
 import { Observable } from "rxjs";
+import { environment } from '../environments/environment'
+
+const apiUrl = environment.apiUrl;
 
 
 @Injectable()
@@ -8,6 +11,9 @@ export class AppInterceptor implements HttpInterceptor {
     constructor() { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if (req.url.startsWith('/api')) {
+            req = req.clone({url: req.url.replace('/api', apiUrl)});
+        }
         return next.handle(req);
     }
 
